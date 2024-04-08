4d411c4d06a56e1ab23178b7d3bfae3e02f99f3c
Neelectric <Neel.R@web.de>
2024-04-08 15:51:06 +0100

    Intersecting lines works!

--- geom/LineSegmentIntersection.java
+++ geom/LineSegmentIntersection.java
@@ -8,15 +8,20 @@ public class LineSegmentIntersection {
         // Call the intersect method
         Line l1 = new Line();
         Line l2 = new Line();
-        lineSegmentIntersection.intersect(l1, l2);
+        l1.p1 = new Point2D.Double(1, 1);
+        l1.p2 = new Point2D.Double(2, 2);
+        l2.p1 = new Point2D.Double(3, 3);
+        l2.p2 = new Point2D.Double(4, 4);
+        System.out.println("Intersecting lines: " + lineSegmentIntersection.intersect(l1, l2));
     }
 
     private boolean onOppositeSides(Point2D.Double a, Point2D.Double b, Line l) {
         double g, h;
         g = (l.p2.x - l.p1.x) * (a.y - l.p1.y) - (l.p2.y - l.p1.y) * (a.x - l.p1.x);
         h = (l.p2.x - l.p1.x) * (b.y - l.p1.y) - (l.p2.y - l.p1.y) * (b.x - l.p1.x);
-
-        return g * h <= 0.0;
+        boolean onOppositeSides = g * h <= 0.0;
+        System.out.println("onOppositeSides of " + a + " and " + b + " with respect to " + l + " is " + onOppositeSides);
+        return onOppositeSides;
     }
 
     /** returns true if the rectangles containing line segments
@@ -36,7 +41,9 @@ public class LineSegmentIntersection {
         * and top RH corner (x2,y2)
         * Rectangle containing l2 has bottom LH corner (x3,y3)
         * and top RH corner (x4,y4) */
-        return x4 >= x1 && y4 >= y1 && x2 >= x3 && y2 >= y3;
+        boolean boundingBox = x4 >= x1 && y4 >= y1 && x2 >= x3 && y2 >= y3;
+        System.out.println("boundingBox of " + l1 + " and " + l2 + " is " + boundingBox);
+        return boundingBox;
     }
 
     public boolean intersect(Line l1, Line l2) {
@@ -46,7 +53,12 @@ public class LineSegmentIntersection {
         Point2D.Double r = l2.p1;
         Point2D.Double s = l2.p2;
 
-        return (onOppositeSides(p, q, l2) && onOppositeSides(r, s, l1) && boundingBox(p—q, r—s));
-        return true;
+        LineSegment ls1 = new LineSegment();
+        ls1.p1 = p;
+        ls1.p2 = q;
+        LineSegment ls2 = new LineSegment();
+        ls2.p1 = r;
+        ls2.p2 = s;
+        return (onOppositeSides(p, q, l2) && onOppositeSides(r, s, l1) && boundingBox(ls1, ls2));
     }
 }
